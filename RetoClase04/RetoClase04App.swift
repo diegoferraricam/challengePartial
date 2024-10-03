6be2e15e4c6afad9e200d414e7cc2b72d889b7cb
Miguel Vera <pedromvera@bcp.com.pe>
2024-10-03 15:59:04 -0500

    Initial Commit

---
+++ RetoClase04/RetoClase04App.swift
@@ -0,0 +1,17 @@
+//
+//  RetoClase04App.swift
+//  RetoClase04
+//
+//  Created by Miguel Vera on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RetoClase04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
